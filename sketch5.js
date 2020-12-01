c21467664968a08eb843f4c2b364b2b7dcba29a9
anakaren97 <akmartin@sfu.ca>
2020-12-01 12:12:24 -0800

    Update sketch5.js
    updating video effect

--- sketch5.js
+++ sketch5.js
@@ -1,11 +1,18 @@
 let button;
 var video;
+let mic, fft;
+let input;
+let analyzer;
+
+var vScale = 15;
+let back = 51;
+let fillColor = 0;
 
 var vScale = 16;
 let camera;
 function setup() {
-  createCanvas(1465, 800);
-  background(color(random(255),random(255),random(255)));
+  createCanvas(windowWidth, windowHeight);
+  //background(color(random(255),random(255),random(255)));
 
   r = random(255);
   g = random(255);
@@ -36,12 +43,32 @@ function setup() {
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(width / vScale, height / vScale);
+    video.position(width/2, 0);
+    // Create an Audio input
+    input = new p5.AudioIn();
+    analyzer = new p5.Amplitude();
+    input.start();
+    fft = new p5.FFT();
+    fft.setInput(input);
 }
 
-function draw(){
-  background(0);
-  video.loadPixels();
+function draw() {
+  background(back);
 
+  let rms = analyzer.getLevel();
+
+  stroke(random(200,255),fillColor,fillColor);
+  strokeWeight(5);
+
+  let spectrum = fft.analyze();
+    beginShape();
+      for (i = 0; i < spectrum.length; i++) {
+        line(i * 10, map(spectrum[i] * 2, 0, 10, -height/8, 0), width/2, height/2);
+    }
+    endShape();
+
+
+  video.loadPixels();
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       var index = (video.width - x + 1 + (y * video.width)) * 4;
@@ -51,12 +78,24 @@ function draw(){
       var bright = (r + g + b) / 3;
       var w = map(bright, 0, 255, 0, vScale);
       noStroke();
-      fill(r, g, b);
+      fill(fillColor,50,250);
       rectMode(CENTER);
-      rect(x * vScale, y * vScale, w, w);
+      rect(x * vScale, y * vScale, random(20,40),w, random(20,30));
     }
   }
+
+
 }
+function mousePressed() {
+  if (fillColor === 0 && back == 51) {
+    fillColor = random(0,255);
+    back = random(100,255);
+  } else {
+    fillColor =  0;
+    back = 51;
+  }
+}
+
 
 function goBack() {
   // let val = color(random(255),random(255),random(255));
