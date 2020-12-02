498c444ed57a727e1f2a4b5a3a15af69ac9ca96b
Ana Karen <akmartin@sfu.ca>
2020-12-01 22:04:17 -0800

    added text, image, font

--- sketch.js
+++ sketch.js
@@ -1,41 +1,37 @@
 let button;
-let bg;
-let angle = 0;
+let myFont;
+
+function preload() {  myFont = loadFont('assets/hacked.ttf');}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //  cam = createCapture(VIDEO);
-  //  cam.size(20, 20);
-  //  vid = createVideo(['video/glitch.mp4']);
-  //  vid.elt.muted = true;
-  //  vid.loop();
 
-  //bg = loadImage('https://i.ytimg.com/vi/41vuqCTd2-U/maxresdefault.jpg');
-
+//Welcome text
+ fill(43,30,174);
+ textFont(myFont);
+ textSize(36);
+text('Welcome', width/2.5, height/3);
+ loadImage('assets/rect.png', img => {
+   image(img, width/4, height/6);
+ });
+//start button
   button1 = createButton('Start');
-
-  button1.position(width/6, height/8);
+  button1.position(width/2.5, height/1.2);
   button1.mousePressed(page2);
   button1.style("color", "white");
   button1.style("background-color", "transparent");
-  button1.style("padding", "40px 40px");
+  button1.style("padding", "10px 30px");
 
 
 }
 
 function draw(){
-  fill(0,250,0);
-rect(100,100,100,100);
 
 
-//  angle += 0.02;
-//  noStroke();
-  //botton 4
-//  push();
-//  translate(100, height/2);
-//  texture(vid);
-//  pop();
-
+  let s = 'We can all agree this year has been a ride we all deserve a little break so why not join us on a trip that is out of this world with the Magic Van. Think of the Magic Van as a one stop gateway from the taxing day to day life of this global pandemic. When you are ready to start your journey you can press start below!';
+  fill(195,159,192);
+   textSize(26);
+  text(s,width/3.5, height/2, 700,400);
 
 }
 
