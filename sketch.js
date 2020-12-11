cf95b78182181838a28f793229828a2ca027a009
zoe dao-Kroeker <zdaokroe@sfu.ca>
2020-12-10 21:25:08 -0800

    home page button added

--- sketch.js
+++ sketch.js
@@ -6,22 +6,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   //background video
 
-//start button
-  button1 = createButton('Start');
-  button1.position(width/2.5, height/1.1);
-  button1.mousePressed(page2);
-  button1.style("color", "white");
-  button1.style("background-color", "transparent");
-  button1.style("padding", "10px 30px");
-  button1.style("z-index", "10");
-
 
 }
 
 function draw(){
 
-}
-
-function page2() {
-  window.location='page1.html';
 }
